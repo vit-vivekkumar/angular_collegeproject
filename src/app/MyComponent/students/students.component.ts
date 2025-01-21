05d98ba3a3e17aa0629fc27a8caf7b77b449c1a2
Vivek Kumar <vivek964800@gmail.com>
2025-01-21 18:28:29 +0530

    update with @Input & Output and HttpClient API Call

--- src/app/MyComponent/students/students.component.ts
+++ src/app/MyComponent/students/students.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Student } from '../../Student';
 import { CollegeComponent } from '../college/college.component';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-students',
@@ -10,11 +12,18 @@ import { CommonModule } from '@angular/common';
   styleUrl: './students.component.css'
 })
 export class StudentsComponent implements OnInit {
-  
-  @Input() studentList: Student[] = [];  
-
-  constructor() {}
+  @Input() studentList: Student[] = [];
+  constructor(private http: HttpClient) { }
+  @Output() dataFetched = new EventEmitter<any>(); 
+  data=[];
   ngOnInit(): void {
-  }
+    this.getData();
 
+  }
+  public getData() {
+    this.http.get('http://127.0.0.1:8000/api/students/').subscribe((res: any) => {
+      this.data = res;
+      this.dataFetched.emit(this.data);  // Emit data to parent component
+    });
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CollegeComponent } from "./MyComponent/college/college.component";
 import { Student } from './Student';
 import { StudentsComponent } from './MyComponent/students/students.component';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -14,13 +15,27 @@ import { StudentsComponent } from './MyComponent/students/students.component';
 
 export class AppComponent implements OnInit {
   title = 'angular-project';
-  constructor(){
-  }
-  studentList: Student[] = [
-      { id: 1, name: 'Alice', date: '12/12/2024' },
-      { id: 2, name: 'Bob', date: '12/12/2024' },
-      { id: 3, name: 'Charlie', date: '12/12/2026' }
-    ];
+  studentList: Student[] = [];
 
-  ngOnInit(): void {}
+  webhookData: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.checkForUpdates();
+  }
+  // Method to handle the data received from the child component
+  handleDataFetched(data: any) {
+    this.studentList = data;
+    console.log('Received data from child:', this.studentList);
+  }
+
+  checkForUpdates() {
+    // Periodically poll the server for updates
+    setInterval(() => {
+      this.http.get('http://localhost:8000/courses').subscribe(data => {
+        this.webhookData = data;
+      });
+    }, 5000); // Poll every 30 seconds
+  }
 }
